8d033894950574e75462c646e71e157fe0a0fca1
Dominic Tarr <dominic.tarr@gmail.com>
2013-03-15 11:26:42 +1300

    refactor map-reduce test to new api

--- test/map-reduce.js
+++ test/map-reduce.js
@@ -3,41 +3,39 @@ var levelup     = require('levelup')
 var Model       = require('scuttlebutt/model')
 var assert      = require('assert')
 var rimraf      = require('rimraf')
-
-var scuttlebutt = require('..')
+var MapReduce   = require('map-reduce')
+var Scuttlebutt = require('..')
+var SubLevel    = require('level-sublevel')
 
 require('tape')('scuttlebutt: map-reduce', function (t) {
 
   rimraf('/tmp/level-scuttlebutt-example', function () {
     levelup('/tmp/level-scuttlebutt-example', 
       {createIfMissing: true}, function (err, db) {
+
+      SubLevel(db)
   
-      scuttlebutt(db, 'THIS', {
+      Scuttlebutt(db, 'THIS', {
         test: function () {
           return new Model()
         }
       })
 
-      var range = db.scuttlebutt.range
-
-      db.scuttlebutt.addMapReduce({
-        name: 'test',
-        depth: 1,
-        map: function (key, model, emit) {
+      var mapDb = 
+      MapReduce(db, 'test',
+        function (key, model, emit) {
+          model = JSON.parse(model)
           if(!model) return
-          console.log('map', key)
-          emit('square', Math.pow(Number(model.get('number')), 2))
-          emit('cube', Math.pow(Number(model.get('number')), 3))
+          emit('square', Math.pow(Number(model.number), 2))
+          emit('cube', Math.pow(Number(model.number), 3))
         },
-        reduce: function (sum, n) {
+        function (sum, n) {
           return Number(sum) + Number(n)
         },
-        initial: 0
-      })
+        0)
 
       'abcde'.split('').forEach(function (e, i) {
         db.scuttlebutt.open('test-'+e, false, function (err, t) {
-          console.log('open')
           t.set('number', i)
           setTimeout(function () {
             var l = 10
@@ -55,8 +53,8 @@ require('tape')('scuttlebutt: map-reduce', function (t) {
 
       var sq, cu
 
-      db.on('reduce:test', function (group, sum) {
-        console.log('r', [group, sum])
+      mapDb.on('reduce', function (group, sum) {
+        console.log('reduce->', group, sum)
         try {
           assert.deepEqual([['square'], 120], [group, sum])
           console.log('sq')
